1b74d36d5af8bdc792e13c53fddc387789eeebdf
ASendellPrice <ashley.sendell.price@gmail.com>
2024-04-27 21:54:20 +0100

    added fasta extract

--- scripts/extract_sequences.py
+++ scripts/extract_sequences.py
@@ -2,18 +2,25 @@
 File:           merge_pia.py
 Author:         Ashley T. Sendell-Price
 Date:           27.03.2024
-Description:    Takes output from multiple runs of PIA (https://github.com/Allaby-lab/PIA) 
-                and merges into a single tab delimited file with taxonomic information added
-                e.g. kingdom, superphylum, phylum etc. Taxonomic info sourced using taxaranks
-                tool from https://github.com/linzhi2013/taxonomy_ranks
-Usage:          python merge_pia.py PATH/TO/PIA/OUTPUT/DIRECTORY (requires blast_edna env)
+Description:    ADD
+                Requires bioinfokit
+Usage:          python extract_sequences taxaID
 """
 
 #Import required modules
 import sys
+from bioinfokit.analys import Fasta
 
-with open((sys.argv[1]), 'r') as f:
+#Create sequence ID list
+IDfile = open('sequenceIDs.txt', 'w')
+with open(sys.argv[1], 'r') as f:
     for line in f.readlines():
+        #check not a header line
         if not "#" in line:
-            if sys.argv[2] in line:
-                print(line.split(sep = '\t')[0])
+            #
+            if line.split(sep = '\t')[1] == sys.argv[2]:
+                sequenceID = line.split(sep = '\t')[0]
+                IDfile.writelines(sequenceID + '\n')
+
+# extract sequences based on sequence ID and region coordinates
+Fasta.extract_seq(file = FASTA, id="sequenceIDs.txt")
