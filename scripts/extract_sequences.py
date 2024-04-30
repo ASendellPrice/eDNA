af65a1789994e8e391d26ebbf0d2dfde422d98a8
ASendellPrice <ashley.sendell.price@gmail.com>
2024-04-30 20:53:33 +0100

    extract_sequences.py finalised

--- scripts/extract_sequences.py
+++ scripts/extract_sequences.py
@@ -1,10 +1,10 @@
 """
-File:           merge_pia.py
+File:           extract_sequences.py
 Author:         Ashley T. Sendell-Price
-Date:           27.03.2024
-Description:    ADD
+Date:           28.04.2024
+Description:    Outputs sequences assigned to specified taxa ID into a fasta file (output.fasta)
                 Requires bioinfokit
-Usage:          python extract_sequences taxaID
+Usage:          python extract_sequences pia_output fasta taxaID:
 """
 
 #Import required modules
@@ -17,7 +17,7 @@ with open(sys.argv[1], 'r') as f:
     for line in f.readlines():
         #check not a header line
         if not "#" in line:
-            #
+            #If line 
             if line.split(sep = '\t')[1] == sys.argv[3]:
                 sequenceID = line.split(sep = '\t')[0]
                 IDfile.writelines(sequenceID + '\n')
